62f99df665a6870e8eba2ec1aa001b70bf2aa63f
Студент группы IS <is@sielom.local>
2025-11-25 17:52:17 +0500

    commitstartweb

--- webproject/webapp/views.py
+++ webproject/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Client, Booking, Weapon
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def weapons_catalog(request):
+    return render(request, 'weapons_catalog.html')
